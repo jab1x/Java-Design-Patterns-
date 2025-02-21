acd8d7dec736ec8029be65e3f7a1b7c3708b21f3
jab1x <95210658+jab1x@users.noreply.github.com>
2025-02-21 14:01:32 +0200

    Update Product.java

--- lab01_UML/task_3_2/src/Product.java
+++ lab01_UML/task_3_2/src/Product.java
@@ -2,16 +2,4 @@ interface Product {
     void doSomething();
 }
 
-class Product1 implements Product {
-    @Override
-    public void doSomething() {
-        System.out.println("Product1 is doing something");
-    }
-}
 
-class Product2 implements Product {
-    @Override
-    public void doSomething() {
-        System.out.println("Product2 is doing something");
-    }
-}
