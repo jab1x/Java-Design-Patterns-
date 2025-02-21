8301b16069e3d3418a0fa2b6938d653194b77db2
jab1x <95210658+jab1x@users.noreply.github.com>
2025-02-21 15:01:26 +0200

    Create Drinks.java

---
+++ lab02_Fabric_Methods/task_2_3_1/Drinks.java
@@ -0,0 +1,23 @@
+class Espresso implements Coffee {
+    public double getCostPrice() { return 10.0; }
+    public double getSalePrice() { return 25.0; }
+    public String getName() { return "Espresso"; }
+}
+
+class Americano implements Coffee {
+    public double getCostPrice() { return 12.0; }
+    public double getSalePrice() { return 30.0; }
+    public String getName() { return "Americano"; }
+}
+
+class Cappuccino implements Coffee {
+    public double getCostPrice() { return 15.0; }
+    public double getSalePrice() { return 35.0; }
+    public String getName() { return "Cappuccino"; }
+}
+
+class Latte implements Coffee {
+    public double getCostPrice() { return 18.0; }
+    public double getSalePrice() { return 40.0; }
+    public String getName() { return "Latte"; }
+}
