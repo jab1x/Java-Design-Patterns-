d56c447d51c13e74461a375a60eef8f9d56ee0c0
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-05 16:43:31 +0200

    Create LinearEquation.java

---
+++ lab05_Prototype/task_3_1/src/LinearEquation.java
@@ -0,0 +1,22 @@
+class LinearEquation extends Equation {
+    private double a, b;
+
+    public LinearEquation(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public LinearEquation clone() {
+        return new LinearEquation(a, b);
+    }
+
+    @Override
+    public void solve() {
+        if (a != 0) {
+            System.out.println("Solution: x = " + (-b / a));
+        } else {
+            System.out.println("No solution or infinite solutions.");
+        }
+    }
+}
