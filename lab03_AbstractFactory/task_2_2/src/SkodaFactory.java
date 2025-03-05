8d785347a307bcc76d30f597f86b532fe56c5e4b
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-05 16:02:01 +0200

    Create SkodaFactory.java

---
+++ lab03_AbstractFactory/task_2_2/src/SkodaFactory.java
@@ -0,0 +1,16 @@
+class SkodaFactory implements TransportFactory {
+    @Override
+    public Bus createBus() {
+        return new Bus("Skoda", 4500000, 25);
+    }
+    
+    @Override
+    public Tram createTram() {
+        return new Tram("Skoda", 9000000, 8);
+    }
+    
+    @Override
+    public Trolleybus createTrolleybus() {
+        return new Trolleybus("Skoda", 6800000, 12);
+    }
+}
