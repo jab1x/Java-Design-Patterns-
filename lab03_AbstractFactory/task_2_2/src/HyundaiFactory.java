f65cb5f01ceebf8f34ee7592a2ebd0e9cea5d056
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-05 16:02:21 +0200

    Create HyundaiFactory.java

---
+++ lab03_AbstractFactory/task_2_2/src/HyundaiFactory.java
@@ -0,0 +1,16 @@
+class HyundaiFactory implements TransportFactory {
+    @Override
+    public Bus createBus() {
+        return new Bus("Hyundai", 5500000, 20);
+    }
+    
+    @Override
+    public Tram createTram() {
+        return new Tram("Hyundai", 9500000, 6);
+    }
+    
+    @Override
+    public Trolleybus createTrolleybus() {
+        return new Trolleybus("Hyundai", 7000000, 11);
+    }
+}
