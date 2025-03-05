20120339586bd254da8cfafdec589fba0d1c640f
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-05 16:36:52 +0200

    Create Transmission.java

---
+++ lab05_Prototype/task_3_2/src/Transmission.java
@@ -0,0 +1,22 @@
+class Transmission implements Cloneable {
+    private String type;
+    private int gears;
+
+    public Transmission(String type, int gears) {
+        this.type = type;
+        this.gears = gears;
+    }
+
+    @Override
+    protected Transmission clone() throws CloneNotSupportedException {
+        return (Transmission) super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Transmission{" +
+                "type='" + type + '\'' +
+                ", gears=" + gears +
+                '}';
+    }
+}
