8b5187c7b27cc9ff67165b5d9cb8fb122dde9daf
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-05 16:43:51 +0200

    Create QuadraticEquation.java

---
+++ lab05_Prototype/task_3_1/src/QuadraticEquation.java
@@ -0,0 +1,29 @@
+class QuadraticEquation extends Equation {
+    private double a, b, c;
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public QuadraticEquation clone() {
+        return new QuadraticEquation(a, b, c);
+    }
+
+    @Override
+    public void solve() {
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant > 0) {
+            double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("Solutions: x1 = " + x1 + ", x2 = " + x2);
+        } else if (discriminant == 0) {
+            double x = -b / (2 * a);
+            System.out.println("Solution: x = " + x);
+        } else {
+            System.out.println("No real solutions.");
+        }
+    }
+}
