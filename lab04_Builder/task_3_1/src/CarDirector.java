4548bc3d757742ddf6050988c5b916b58500f63e
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-05 16:21:11 +0200

    Create CarDirector.java

---
+++ lab04_Builder/task_3_1/src/CarDirector.java
@@ -0,0 +1,21 @@
+class CarDirector {
+    public Car buildSUV() {
+        return new CarBuilder()
+                .setBodyType("SUV")
+                .setEngine(new Engine(250, 2.5, 320, "Petrol"))
+                .setTransmission(new Transmission("Automatic", 8))
+                .setWheels(new Wheels("Alloy", 18))
+                .setColor("Black")
+                .build();
+    }
+
+    public Car buildSedan() {
+        return new CarBuilder()
+                .setBodyType("Sedan")
+                .setEngine(new Engine(180, 2.0, 260, "Diesel"))
+                .setTransmission(new Transmission("Manual", 6))
+                .setWheels(new Wheels("Steel", 16))
+                .setColor("White")
+                .build();
+    }
+}
