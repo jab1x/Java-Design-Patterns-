d6e5c429491d850faf77540d0a1cf4a4a4d3c499
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-05 16:45:11 +0200

    Create EquationPrototypeDemo.java

---
+++ lab05_Prototype/task_3_1/src/EquationPrototypeDemo.java
@@ -0,0 +1,19 @@
+// як приклад роботи
+public class EquationPrototypeDemo { 
+    public static void main(String[] args) {
+        LinearEquation linear = new LinearEquation(2, 5);
+        QuadraticEquation quadratic = new QuadraticEquation(1, -3, 2);
+
+        System.out.println("Solving original equations:");
+        linear.solve();
+        quadratic.solve();
+
+        // клонування рівнянь
+        LinearEquation clonedLinear = linear.clone();
+        QuadraticEquation clonedQuadratic = quadratic.clone();
+
+        System.out.println("Solving cloned equations:");
+        clonedLinear.solve();
+        clonedQuadratic.solve();
+    }
+}
