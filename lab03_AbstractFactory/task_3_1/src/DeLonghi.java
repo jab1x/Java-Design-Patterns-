3e00907b3fbb1bd2d4d0ad0c84dc25a65055b367
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-05 16:08:53 +0200

    Create DeLonghi.java

---
+++ lab03_AbstractFactory/task_3_1/src/DeLonghi.java
@@ -0,0 +1,10 @@
+class DeLonghi extends CoffeeMachine {
+    public DeLonghi() {
+        super("DeLonghi", 20000, 500, 10, 50);
+    }
+
+    @Override
+    public void displayInfo(int days, int cupsPerDay) {
+        System.out.println("DeLonghi: Прибуток за " + days + " днів: " + calculateProfit(days, cupsPerDay));
+    }
+}
