a5f5b3d72a05bd2da93910d69673cd14dc294eaf
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-05 16:27:36 +0200

    Create StringBuilderCustom.java

---
+++ lab04_Builder/task_3_2/src/StringBuilderCustom.java
@@ -0,0 +1,24 @@
+class StringBuilderCustom {
+    private StringBuilder builder;
+
+    public StringBuilderCustom() {
+        this.builder = new StringBuilder();
+    }
+
+    public StringBuilderCustom append(String sequence) {
+        builder.append(sequence);
+        return this;
+    }
+
+    public StringBuilderCustom insert(int position, String sequence) {
+        if (position < 0 || position > builder.length()) {
+            throw new IndexOutOfBoundsException("Invalid position");
+        }
+        builder.insert(position, sequence);
+        return this;
+    }
+
+    public String build() {
+        return builder.toString();
+    }
+}
