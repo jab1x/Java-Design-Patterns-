cb0aa1858b2181f4a479bc9e8d358be4cca9ba32
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-07 21:10:57 +0200

    Update Drinks.java

--- lab02_Fabric_Methods/task_2_3_1/Drinks.java
+++ lab02_Fabric_Methods/task_2_3_1/Drinks.java
@@ -1,22 +1,22 @@
-class Espresso implements Coffee {
+public class Espresso implements Coffee {
     public double getCostPrice() { return 10.0; }
     public double getSalePrice() { return 25.0; }
     public String getName() { return "Espresso"; }
 }
 
-class Americano implements Coffee {
+public class Americano implements Coffee {
     public double getCostPrice() { return 12.0; }
     public double getSalePrice() { return 30.0; }
     public String getName() { return "Americano"; }
 }
 
-class Cappuccino implements Coffee {
+public class Cappuccino implements Coffee {
     public double getCostPrice() { return 15.0; }
     public double getSalePrice() { return 35.0; }
     public String getName() { return "Cappuccino"; }
 }
 
-class Latte implements Coffee {
+public class Latte implements Coffee {
     public double getCostPrice() { return 18.0; }
     public double getSalePrice() { return 40.0; }
     public String getName() { return "Latte"; }
