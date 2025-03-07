a765df058d9484acfc92eb5342f7a74479988d57
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-07 21:13:23 +0200

    Update Products.java

--- lab01_UML/task_3_2/src/Products.java
+++ lab01_UML/task_3_2/src/Products.java
@@ -1,11 +1,11 @@
-class Product1 implements Product {
+public class Product1 implements Product {
     @Override
     public void doSomething() {
         System.out.println("Product1 is doing something");
     }
 }
 
-class Product2 implements Product {
+public class Product2 implements Product {
     @Override
     public void doSomething() {
         System.out.println("Product2 is doing something");
