9689988561a5c56eb6c038dafaa6bd878e27656f
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-07 20:45:45 +0200

    Create USBTypeCToMicroUSBAdapter.java

---
+++ lab07_Adapter/task_3_1/src/USBTypeCToMicroUSBAdapter.java
@@ -0,0 +1,13 @@
+class USBTypeCToMicroUSBAdapter implements MicroUSB {
+    private USBTypeC usbTypeCCharger;
+
+    public USBTypeCToMicroUSBAdapter(USBTypeC usbTypeCCharger) {
+        this.usbTypeCCharger = usbTypeCCharger;
+    }
+
+    @Override
+    public void chargeWithMicroUSB() {
+        System.out.println("Адаптація USB Type-C до micro-USB");
+        usbTypeCCharger.chargeWithUSBTypeC();
+    }
+}
