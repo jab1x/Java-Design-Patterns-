d736853cc71745af8881460e46663f91f7f3c906
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-07 19:37:59 +0200

    Create Transmission.java

---
+++ lab06_Singleton/task_3_2/src/Transmission.java
@@ -0,0 +1,17 @@
+class Transmission {
+    private String type;
+    private int gears;
+
+    public Transmission(String type, int gears) {
+        this.type = type;
+        this.gears = gears;
+    }
+
+    @Override
+    public String toString() {
+        return "Transmission{" +
+                "type='" + type + '\'' +
+                ", gears=" + gears +
+                '}';
+    }
+}
