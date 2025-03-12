078b8924e3e4ff9fd2fa271d6faa6248f8520f3f
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-12 14:46:31 +0200

    Update Supermarket.java

--- lab09_Composite(task_3_1)/src/Supermarket.java
+++ lab09_Composite(task_3_1)/src/Supermarket.java
@@ -1,27 +1,24 @@
 public class Supermarket {
     public static void main(String[] args) {
         
-        Manager director = new Manager("Alice", 5000);
+        Manager director = new Manager("Dmitrii", 5000);
 
-        // відділи магазину
-        Manager meatDeptManager = new Manager("John (Meat Dept Manager)", 3000);
-        Manager dairyDeptManager = new Manager("Emma (Dairy Dept Manager)", 3000);
-        Manager bakeryDeptManager = new Manager("Sophia (Bakery Dept Manager)", 3000);
+        Manager meatDeptManager = new Manager("Petro (meat dept manager)", 3000);
+        Manager dairyDeptManager = new Manager("Nastya (meat dept manager)", 3000);
+        Manager bakeryDeptManager = new Manager("Sophia (meat dept manager)", 3000);
 
-        // менеджери
-        meatDeptManager.addEmployee(new Salesperson("Bob (Meat Seller)", 2000));
-        meatDeptManager.addEmployee(new Salesperson("Mike (Meat Seller)", 2000));
-        meatDeptManager.addEmployee(new Salesperson("Anna (Meat Seller)", 2000));
+        meatDeptManager.addEmployee(new Salesperson("Victor (meat seller)", 2000));
+        meatDeptManager.addEmployee(new Salesperson("Oleg (meat seller)", 2000));
+        meatDeptManager.addEmployee(new Salesperson("Anna (meat seller)", 2000));
 
-        dairyDeptManager.addEmployee(new Salesperson("Sarah (Dairy Seller)", 1800));
-        dairyDeptManager.addEmployee(new Salesperson("David (Dairy Seller)", 1800));
-        dairyDeptManager.addEmployee(new Salesperson("Tom (Dairy Seller)", 1800));
+        dairyDeptManager.addEmployee(new Salesperson("Sergii (dairy seller)", 1800));
+        dairyDeptManager.addEmployee(new Salesperson("Illya (dairy seller)", 1800));
+        dairyDeptManager.addEmployee(new Salesperson("Karina (dairy seller)", 1800));
 
-        bakeryDeptManager.addEmployee(new Salesperson("Linda (Bakery Seller)", 1900));
-        bakeryDeptManager.addEmployee(new Salesperson("Karen (Bakery Seller)", 1900));
-        bakeryDeptManager.addEmployee(new Salesperson("Paul (Bakery Seller)", 1900));
+        bakeryDeptManager.addEmployee(new Salesperson("Oleksii (bakery seller)", 1900));
+        bakeryDeptManager.addEmployee(new Salesperson("Pavlo (bakery seller)", 1900));
+        bakeryDeptManager.addEmployee(new Salesperson("Mark (bakery seller)", 1900));
 
-        // відділи, якими керує директор
         director.addEmployee(meatDeptManager);
         director.addEmployee(dairyDeptManager);
         director.addEmployee(bakeryDeptManager);
