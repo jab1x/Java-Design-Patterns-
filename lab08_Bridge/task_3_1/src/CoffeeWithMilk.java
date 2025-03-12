1666709085f39daaf0f800db1d7b17fbc5d990b2
jab1x <95210658+jab1x@users.noreply.github.com>
2025-03-12 14:18:00 +0200

    Create CoffeeWithMilk.java

---
+++ lab08_Bridge/task_3_1/src/CoffeeWithMilk.java
@@ -0,0 +1,20 @@
+public class CoffeeWithMilk extends Coffee {
+    private final int milkVolume;
+
+    public CoffeeWithMilk(int sugar, int milkVolume, ServingType servingType) {
+        super(sugar, servingType);
+        this.milkVolume = milkVolume;
+    }
+
+    @Override
+    public void prepare() {
+        super.prepare();
+        System.out.printf("Adding milk: %s ml...%n", milkVolume);
+        if (sugar > 0) System.out.printf("Adding sugar: %s pieces...%n", sugar);
+    }
+
+    @Override
+    public int cost() {
+        return super.cost() + (int) (milkVolume / 20.0);
+    }
+}
