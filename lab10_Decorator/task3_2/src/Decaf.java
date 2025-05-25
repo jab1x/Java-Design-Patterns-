7fa5d6a7dec259197652f6cdc2fb005c24b0126e
jab1x <95210658+jab1x@users.noreply.github.com>
2025-05-25 17:43:18 +0300

    Create Decaf.java

---
+++ lab10_Decorator/task3_2/src/Decaf.java
@@ -0,0 +1,10 @@
+public class Decaf extends Beverage {
+    @Override
+    public String description() {
+        return "Decaf Coffee";
+    }
+    @Override
+    public double cost() {
+        return 1.05;
+    }
+}
