ffa6fec674bcc783517770cb4dabb9c0766f5b29
jab1x <95210658+jab1x@users.noreply.github.com>
2025-05-25 17:43:35 +0300

    Create DarkRoast.java

---
+++ lab10_Decorator/task3_2/src/DarkRoast.java
@@ -0,0 +1,10 @@
+public class DarkRoast extends Beverage {
+    @Override
+    public String description() {
+        return "Dark Roast Coffee";
+    }
+    @Override
+    public double cost() {
+        return 0.99;
+    }
+}
