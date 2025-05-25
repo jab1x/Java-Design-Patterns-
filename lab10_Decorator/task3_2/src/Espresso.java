ced88b69745874ab6393c1d6637d65239e444e77
jab1x <95210658+jab1x@users.noreply.github.com>
2025-05-25 17:43:50 +0300

    Create Espresso.java

---
+++ lab10_Decorator/task3_2/src/Espresso.java
@@ -0,0 +1,10 @@
+public class Espresso extends Beverage {
+    @Override
+    public String description() {
+        return "Espresso";
+    }
+    @Override
+    public double cost() {
+        return 1.99;
+    }
+}
