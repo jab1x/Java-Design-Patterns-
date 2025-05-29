524484e363f8b0017766c8fd79a9cdd0cf42e01c
jab1x <95210658+jab1x@users.noreply.github.com>
2025-05-29 19:24:59 +0300

    Create  Shortcut.java

---
+++ lab15_Command/task_3_2/src/Shortcut.java
@@ -0,0 +1,9 @@
+public class Shortcut {
+    private Command command;
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+    public void press() {
+        if (command != null) command.execute();
+    }
+}
