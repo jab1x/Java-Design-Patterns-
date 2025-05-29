c8303e91456490e846bf623c32aa3933dab5a675
jab1x <95210658+jab1x@users.noreply.github.com>
2025-05-29 19:32:34 +0300

    Create  NextChannelCommand.java

---
+++ lab15_Command/task_3_3/src/NextChannelCommand.java
@@ -0,0 +1,10 @@
+public class NextChannelCommand implements Command {
+    private final Device device;
+    public NextChannelCommand(Device device) {
+        this.device = device;
+    }
+    @Override
+    public void execute() {
+        device.nextChanel();
+    }
+}
