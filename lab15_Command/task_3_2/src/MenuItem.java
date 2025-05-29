af046a814267640fac5a4be584ef086e9851dc2a
jab1x <95210658+jab1x@users.noreply.github.com>
2025-05-29 19:25:15 +0300

    Create  MenuItem.java

---
+++ lab15_Command/task_3_2/src/MenuItem.java
@@ -0,0 +1,9 @@
+public class MenuItem {
+    private Command command;
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+    public void select() {
+        if (command != null) command.execute();
+    }
+}
