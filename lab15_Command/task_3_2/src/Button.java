f67b419c59c62ee144d92be70c1569d18f01a666
jab1x <95210658+jab1x@users.noreply.github.com>
2025-05-29 19:25:28 +0300

    Create  Button.java

---
+++ lab15_Command/task_3_2/src/Button.java
@@ -0,0 +1,9 @@
+public class Button {
+    private Command command;
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+    public void click() {
+        if (command != null) command.execute();
+    }
+}
