069d5dff38238a40367360092ce8a6b5c1e23091
jab1x <95210658+jab1x@users.noreply.github.com>
2025-05-29 19:32:07 +0300

    Create  VolumeUpCommand.java

---
+++ lab15_Command/task_3_3/src/VolumeUpCommand.java
@@ -0,0 +1,10 @@
+public class VolumeUpCommand implements Command {
+    private final Device device;
+    public VolumeUpCommand(Device device) {
+        this.device = device;
+    }
+    @Override
+    public void execute() {
+        device.volumeUp();
+    }
+}
