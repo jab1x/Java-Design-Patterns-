8fe744902e6a29ca6fd041bdb05553ecfcefbff2
jab1x <95210658+jab1x@users.noreply.github.com>
2025-05-29 19:54:41 +0300

    Create  PlanesOnGround.java

---
+++ lab17_Mediator/task_3_1/src/PlanesOnGround.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PlanesOnGround {
+    private final List<Plane> planes = new ArrayList<>();
+
+    public void addPlane(Plane p) {
+        planes.add(p);
+    }
+
+    public void removePlane(Plane p) {
+        planes.remove(p);
+    }
+
+    public List<Integer> getPlaneIds() {
+        return planes.stream().map(Plane::getId).collect(Collectors.toList());
+    }
+}
