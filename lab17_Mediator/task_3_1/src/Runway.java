8f896f7167a6e0db292b3f892225e1d74a3df0f9
jab1x <95210658+jab1x@users.noreply.github.com>
2025-05-29 19:54:15 +0300

    Create  Runway.java

---
+++ lab17_Mediator/task_3_1/src/Runway.java
@@ -0,0 +1,11 @@
+public class Runway {
+    private boolean available = true;
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean free) {
+        this.available = free;
+    }
+}
