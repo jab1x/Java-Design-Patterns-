600c400386906b4ceda36d30119587223e8a93c1
jab1x <95210658+jab1x@users.noreply.github.com>
2025-05-31 12:57:51 +0300

    Create  Calculator.java

---
+++ lab21_Strategy/task_3_2/src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    
+    private OperationStrategy strategy;
+
+
+    public void setStrategy(OperationStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    
+    public int calculate(int a, int b) {
+        if (strategy == null) {
+            throw new IllegalStateException("Operation strategy not set");
+        }
+        return strategy.execute(a, b);
+    }
+}
